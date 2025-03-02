2d48d5616310f25dced9e9ee457faf190782d40a
Viliyan Raychov <vvr.vichos@gmail.com>
2025-03-02 19:53:09 +0200

    Adding enums for Gender and Report Reason

---
+++ Data/Freedle.Data.Models/ReportReason.cs
@@ -0,0 +1,16 @@
+﻿namespace Freedle.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum ReportReason
+    {
+        NotInterested = 1,
+        Spam = 2,
+        SensitiveContent = 3,
+        AbusiveOrHarmful = 4,
+    }
+}
--- Data/Freedle.Data.Models/obj/Debug/net7.0/Freedle.Data.Models.AssemblyInfo.cs
+++ Data/Freedle.Data.Models/obj/Debug/net7.0/Freedle.Data.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Freedle.Data.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422069f6e5223c799ba941a9c9d29acac0730392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1280f706841474a1e2652a257ba64994fdeeb780")]
 [assembly: System.Reflection.AssemblyProductAttribute("Freedle.Data.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Freedle.Data.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

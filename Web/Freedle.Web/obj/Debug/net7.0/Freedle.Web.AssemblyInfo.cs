91cd6403df2b855669a7cddfb9cb430fd8ce91e3
Viliyan Raychov <vvr.vichos@gmail.com>
2025-03-07 12:10:52 +0200

    Adding navbar spacing and hover effect

--- Web/Freedle.Web/obj/Debug/net7.0/Freedle.Web.AssemblyInfo.cs
+++ Web/Freedle.Web/obj/Debug/net7.0/Freedle.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac1813ba4fa8290766c7cb0ea46cb04893d75cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0556599a798dabd52c1b3eafabe3a30ee44ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

aaafa5802c70f5548004b460fa06ca84aea0fc23
Viliyan Raychov <vvr.vichos@gmail.com>
2025-03-22 23:47:49 +0200

    Add message timestamp display for sent messages.

--- Web/Freedle.Web/obj/Debug/net7.0/Freedle.Web.AssemblyInfo.cs
+++ Web/Freedle.Web/obj/Debug/net7.0/Freedle.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc95688a9d67793dcdc0c237fd7c5a8497c2166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d26b0474f37b40f405c1803ec0e88fee718b2ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

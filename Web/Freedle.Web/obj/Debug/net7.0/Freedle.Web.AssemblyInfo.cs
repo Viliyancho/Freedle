74043561ac991fcc465f58dd758b098f15c3c7fd
Viliyan Raychov <vvr.vichos@gmail.com>
2025-03-27 23:12:56 +0200

    Added Folder With Pictures

--- Web/Freedle.Web/obj/Debug/net7.0/Freedle.Web.AssemblyInfo.cs
+++ Web/Freedle.Web/obj/Debug/net7.0/Freedle.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0194290b8820a827e475c6543d681dfc6923672e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c637608f3b5a915d8671ea8be653f50496fc60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Freedle.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
